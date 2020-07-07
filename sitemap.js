f7d5245fce3907e42a16fcf43d9892ba4fab02c6
Steely Wing <steely.wing@gmail.com>
2020-07-07 14:53:12 +0800

    Update sitemap

--- sitemap.js
+++ sitemap.js
@@ -1,43 +1,57 @@
-const fs = require('fs');
-const path = require('path');
-const md = require('markdown-it')();
 const glob = require('glob');
+const path = require('path');
+const fs = require('fs');
 const HOSTNAME = 'https://steelywing.github.io/';
 const BASE_URL = '/note/#/';
-let sidebars = [];
 
-function getMarkdownLinks(tokens) {
-    let links = [];
-
-    tokens.forEach((token) => {
-        if (Array.isArray(token.attrs)) {
-            token.attrs.forEach((attr) => {
-                if (attr[0] === 'href') {
-                    links.push(attr[1]);
-                }
-            })
+class Sidebar {
+    constructor(file) {
+        if (typeof file === 'string') {
+            this.load(file);
         }
-        if (Array.isArray(token.children)) {
-            links = links.concat(getMarkdownLinks(token.children));
+    }
+
+    load(file) {
+        const md = require('markdown-it')();
+
+        this.file = path.resolve(file);
+        this.dir = path.dirname(this.file);
+
+        this.tokens = md.parse(
+            fs.readFileSync(this.file, { encoding: 'utf8' })
+        );
+    }
+
+    static extractLinks(tokens) {
+        let links = [];
+
+        tokens.forEach((token) => {
+            if (Array.isArray(token.attrs)) {
+                token.attrs.forEach((attr) => {
+                    if (attr[0] === 'href') {
+                        links.push(attr[1]);
+                    }
+                })
+            }
+            if (Array.isArray(token.children)) {
+                links = links.concat(this.extractLinks(token.children));
+            }
+        });
+
+        return links;
+    }
+
+    getLinks() {
+        if (!Array.isArray(this.tokens)) {
+            return [];
         }
-    });
-    return links;
-}
 
-function loadSidebar(sidebar) {
-    sidebar = path.resolve(sidebar);
-    let dir = path.dirname(sidebar);
-    let links = [];
+        let links = this.constructor.extractLinks(this.tokens);
 
-    let file = fs.readFileSync(sidebar, { encoding: 'utf8' });
-    let tokens = md.parse(file);
-    links = getMarkdownLinks(tokens);
-    console.log(links);
-    return links.map((link) => {
-        return path.relative('', path.resolve(dir, link));
-    });
-
-    return links;
+        return links.map((link) => {
+            return path.relative('', path.resolve(this.dir, link));
+        });
+    }
 }
 
 new Promise((resolve, reject) => {
@@ -53,7 +67,8 @@ new Promise((resolve, reject) => {
     const stream = new SitemapStream({ hostname: HOSTNAME })
     sidebars.forEach((sidebar) => {
         console.log('Reading ' + sidebar);
-        links = loadSidebar(sidebar);
+        links = (new Sidebar(sidebar)).getLinks();
+        console.log(links);
         links.forEach(link => {
             link = link.replace(/\\/g, '/');
             stream.write({ url: BASE_URL + link })
